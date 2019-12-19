d8d5fe2a5e9126eea756aa23161484e38b013855
Adam <Adam>
2019-12-19 19:23:51 +0100

    Add animation of unlocking quotes (quotes not added or displayed yet) and add responsive to bottom nav bar

--- lib/screens/dumpling_screen.dart
+++ lib/screens/dumpling_screen.dart
@@ -1,15 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quoty_dumpling_app/helpers/size_config.dart';
+import 'package:quoty_dumpling_app/providers/dumpling_provider.dart';
+import 'package:quoty_dumpling_app/widgets/custom_app_bar.dart';
 import 'package:quoty_dumpling_app/widgets/dumpling.dart';
-import 'package:quoty_dumpling_app/widgets/dumpling_screen_app_bar.dart';
 import 'package:quoty_dumpling_app/widgets/progress_bar.dart';
 
-class DumplingScreen extends StatelessWidget {
+class DumplingScreen extends StatefulWidget {
   static const routeId = 'dumpling-screen';
 
   @override
+  _DumplingScreenState createState() => _DumplingScreenState();
+}
+
+class _DumplingScreenState extends State<DumplingScreen>
+    with TickerProviderStateMixin {
+  Animation<double> _dumplingAnimation;
+  Animation<double> _initAnimation;
+  Animation<Offset> _newQuoteSlideAnimation;
+  AnimationController _dumplingAnimController;
+  AnimationController _initAnimController;
+  var _isFull = false;
+  var _isInit = true;
+
+  @override
+  void initState() {
+    super.initState();
+    //
+    _dumplingAnimController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 250),
+    );
+    //
+    _initAnimController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 150),
+    );
+    //
+    _dumplingAnimation = Tween<double>(
+      begin: 1,
+      end: 0,
+    ).animate(
+      CurvedAnimation(
+        curve: Curves.easeIn,
+        parent: _dumplingAnimController,
+      ),
+    );
+    //
+    _initAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(
+      CurvedAnimation(
+        curve: Curves.easeIn,
+        parent: _initAnimController,
+      ),
+    );
+    //
+    _newQuoteSlideAnimation = Tween<Offset>(
+      begin: Offset(0, 10),
+      end: Offset(0, -10),
+    ).animate(
+      CurvedAnimation(
+        curve: Curves.easeIn,
+        parent: _dumplingAnimController,
+      ),
+    );
+    _initAnimController.forward();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_isInit) {
+      SizeConfig().init(context);
+      _isInit = false;
+    }
+    // when the progress bar is full dumpling fades out and new_quote widget shows smoothly
+    if (Provider.of<DumplingProvider>(context).isFull) {
+      _dumplingAnimController.forward();
+      Future.delayed(Duration(milliseconds: 250), () {
+        setState(() {
+          _isFull = true;
+          _dumplingAnimController.reverse();
+        });
+      }).then((_) =>
+          Provider.of<DumplingProvider>(context).clearClickingProgress());
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _dumplingAnimController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -28,11 +115,29 @@ class DumplingScreen extends StatelessWidget {
             children: <Widget>[
               CustomAppBar(),
               SizedBox(
-                height: SizeConfig.screenHeight * 0.09,
+                height: SizeConfig.screenHeight * 0.1,
               ),
               //TODO: Show dialog and other widget if dumpling is opened. (unlocked quote widget);
-              Dumpling(),
-              ProgressBar(),
+              Center(
+                child: FadeTransition(
+                  opacity: _initAnimation,
+                  child: FadeTransition(
+                    opacity: _dumplingAnimation,
+                    child: !_isFull
+                        ? Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Dumpling(),
+                              ProgressBar(),
+                            ],
+                          )
+                        : SlideTransition(
+                            position: _newQuoteSlideAnimation,
+                            child: Text('full'),
+                          ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
--- lib/widgets/progress_bar.dart
+++ lib/widgets/progress_bar.dart
@@ -8,40 +8,55 @@ import 'package:quoty_dumpling_app/providers/dumpling_provider.dart';
 class ProgressBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white70.withOpacity(.85),
-      clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Stack(
-        children: <Widget>[
-          Container(
-            width: SizeConfig.screenWidth * .7,
-            height: SizeConfig.screenHeight * 0.02,
-          ),
-          AnimatedContainer(
-            duration: Duration(milliseconds: 100),
-            width: min(
-              SizeConfig.screenWidth *
-                  .7 *
-                  Provider.of<DumplingProvider>(context)
-                      .progressBarStatus, // if not all space is taken
-              SizeConfig.screenWidth * .7, // it is taken
-            ),
-            height: SizeConfig.screenHeight * 0.02,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color(0xff91bd3a),
-                  Theme.of(context).buttonColor.withRed(150).withGreen(240),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Theme.of(context)
+                .buttonColor
+                .withRed(150)
+                .withGreen(240)
+                .withOpacity(.4),
+            blurRadius: 8,
           ),
         ],
+      ),
+      child: Card(
+        color: Colors.white70.withOpacity(.85),
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: SizeConfig.screenWidth * .7,
+              height: SizeConfig.screenHeight * 0.02,
+            ),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 100),
+              width: min(
+                SizeConfig.screenWidth *
+                    .7 *
+                    Provider.of<DumplingProvider>(context)
+                        .progressBarStatus, // if not all space is taken
+                SizeConfig.screenWidth * .7, // it is taken
+              ),
+              height: SizeConfig.screenHeight * 0.02,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xff91bd3a),
+                    Theme.of(context).buttonColor.withRed(150).withGreen(240),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
