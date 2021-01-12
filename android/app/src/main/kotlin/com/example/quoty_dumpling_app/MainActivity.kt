341acd8a1640d2afb621e83a561382728ebc0ad3
Adam Podkowinski <adampodkev@gmail.com>
2021-01-12 17:12:22 +0100

    Repair bug with clipboard in collection

--- android/app/src/main/kotlin/com/example/quoty_dumpling_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/quoty_dumpling_app/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.quoty_dumpling_app
 
-import androidx.annotation.NonNull;
+import android.content.ClipData
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val primaryClipDescription = clipboard.primaryClipDescription
+        val clip: ClipData = ClipData.newPlainText("Label", "Value")
+        if (clipboard.hasPrimaryClip() && (primaryClipDescription == null || !primaryClipDescription.hasMimeType(MIMETYPE_TEXT_PLAIN))) {
+            clipboard.primaryClip = clip
+            Log.i("MainActivityTag", "Clipboard Updated")
+        } else if (!clipboard.hasPrimaryClip()) {
+            // Only for Stable Channel. PrimaryClip NPE Handled in Master channel already
+            clipboard.primaryClip = clip
+            Log.i("MainActivityTag", "Clipboard is Empty...Updated")
+        } else {
+            Log.i("MainActivityTag", "Clipboard Safe")
+        }
     }
 }
