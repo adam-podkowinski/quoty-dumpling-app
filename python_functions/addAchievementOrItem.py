a4ad5e86d5fd72e8798a18914bf92b983da610d1
Adam Podkowinski <adampodkdev@gmail.com>
2021-03-13 21:29:54 +0100

    Start working at add item script

--- python_functions/addAchievementOrItem.py
+++ python_functions/addAchievementOrItem.py
@@ -3,15 +3,14 @@ from os import path
 
 basepath = path.dirname(__file__)
 
-choice = input("What do you wanna add?\n1. Achievement\n2.Item(not implemented yet)\n")
+choice = input(
+    "What do you wanna add?\n1. Achievement\n2.Item(not implemented yet)\n")
 
 filename = ""
 if choice == "1":
     filename = "achievements"
 else:
     filename = "items"
-    print("Ending program...\n")
-    exit(0)
 
 filepath = path.abspath(
     path.join(basepath, "..", "assets", filename, filename + ".json"))
@@ -52,6 +51,42 @@ if choice == "1":
         {"id": nextId, "title": title, "description": description, "maxToDoVal": int(maxToDoVal),
          "billsReward": billsReward, "diamondsReward": diamondsReward})
     print(data)
+
+elif choice == "2":
+    name = input("\nName: ")
+    description = input("\nDescription: ")
+    item_type_inp = input("""
+What's the item type (input a number)
+1) upgrade
+2) powerup
+3) money
+Your choose: """)
+    switcher = {
+        "1": "upgrade",
+        "2": "powerup",
+        "3": "money"
+    }
+    item_type = switcher.get(item_type_inp, "Invalid type!")
+    use_case_inp = input("""
+What's the use case for your item (input a number):
+1) billsOnClick
+2) clickMultiplier
+3) billsOpening
+4) bills""")
+    switcher = {
+        "1": "billsOnClick",
+        "2": "clickMultiplier",
+        "3": "billsOpening",
+        "4": "bills"
+    }
+    useCase = switcher.get(use_case_inp, "Invalid use case!")
+    default_price_bills = input("Default bills price: ")
+    default_price_diamonds = input("Default diamonds price: ")
+    price_usd = input("Default USD price: ")
+    use_tim = input("Use time: ")
+
+    exit(0)
+
 
 with open(filepath, 'w') as f:
     json.dump(data, f, indent=2)
