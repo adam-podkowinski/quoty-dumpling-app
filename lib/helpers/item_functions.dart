8e46ce68c18645e2fcee35ffd7ac27db84f6c006
Adam Podkowinski <adampodkdev@gmail.com>
2021-03-18 18:48:05 +0100

    Add items

--- lib/helpers/item_functions.dart
+++ lib/helpers/item_functions.dart
@@ -2,33 +2,49 @@ import 'package:provider/provider.dart';
 import 'package:quoty_dumpling_app/providers/dumpling_provider.dart';
 import 'package:quoty_dumpling_app/providers/shop.dart';
 
+const onBuyFunction000 = 1;
+const onBuyFunction001 = 0.005;
+const onBuyFunction002 = 0.05;
+const onBuyFunction007 = 0.04;
+const onBuyFunction008 = 20;
+const onBuyFunction009 = 0.1;
+
+const onBuyFunction003 = 10;
+const onBuyFunction004 = 0.6;
+const onBuyFunction005 = 3.0;
+
 Map<String, Function> itemFunctions = {
   // Upgrade on buy functions
-  'onBuyFunction000': (context) =>
-      Provider.of<Shop>(context, listen: false).changeBillsOnClick(1),
+  'onBuyFunction000': (context) => Provider.of<Shop>(context, listen: false)
+      .changeBillsOnClick(onBuyFunction000),
   'onBuyFunction001': (context) =>
       Provider.of<DumplingProvider>(context, listen: false)
-          .changeClickMultiplier(.1),
+          .changeClickMultiplier(onBuyFunction001),
   'onBuyFunction002': (context) => Provider.of<Shop>(context, listen: false)
-      .changeCashMultiplierOnOpening(1),
+      .changeCashMultiplierOnOpening(onBuyFunction002),
   'onBuyFunction007': (context) =>
       Provider.of<DumplingProvider>(context, listen: false)
-          .changeClickMultiplier(0.8),
+          .changeClickMultiplier(onBuyFunction007),
+  'onBuyFunction008': (context) => Provider.of<Shop>(context, listen: false)
+      .changeBillsOnClick(onBuyFunction008),
+  'onBuyFunction009': (context) => Provider.of<Shop>(context, listen: false)
+      .changeCashMultiplierOnOpening(onBuyFunction009),
 
   // Powerup on buy functions
   // Double dot is necessary
-  'onBuyFunction003': (context) =>
-      Provider.of<Shop>(context, listen: false)..changeBillsOnClick(10),
+  'onBuyFunction003': (context) => Provider.of<Shop>(context, listen: false)
+    ..changeBillsOnClick(onBuyFunction003),
   'onBuyFunction004': (context) =>
       Provider.of<DumplingProvider>(context, listen: false)
-        ..changeClickMultiplier(1),
+        ..changeClickMultiplier(onBuyFunction004),
   'onBuyFunction005': (context) => Provider.of<Shop>(context, listen: false)
-    ..changeCashMultiplierOnOpening(10),
+    ..changeCashMultiplierOnOpening(onBuyFunction005),
 
   //Undo powerup functions
-  'undoBuyFunction003': (Shop provider) => provider.changeBillsOnClick(-10),
+  'undoBuyFunction003': (Shop provider) =>
+      provider.changeBillsOnClick(-onBuyFunction003),
   'undoBuyFunction004': (DumplingProvider provider) =>
-      provider.changeClickMultiplier(-1),
+      provider.changeClickMultiplier(-onBuyFunction004),
   'undoBuyFunction005': (Shop provider) =>
-      provider.changeCashMultiplierOnOpening(-10),
+      provider.changeCashMultiplierOnOpening(-onBuyFunction005),
 };
--- lib/providers/dumpling_provider.dart
+++ lib/providers/dumpling_provider.dart
@@ -2,10 +2,11 @@ import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DumplingProvider extends ChangeNotifier {
+  static const defaultClickMultiplier = 0.025;
   //shows progress of status bar
   double _progressBarStatus = 0.0;
   //multiplier which shows how much we can add to progressBarStatus each click
-  double _clickMultiplier = 1;
+  double _clickMultiplier = defaultClickMultiplier;
   //if progress bar status is equal to 1 (full)
   var _isFull = false;
 
@@ -22,7 +23,8 @@ class DumplingProvider extends ChangeNotifier {
   Future initDumpling() async {
     final prefs = await SharedPreferences.getInstance();
     _progressBarStatus = prefs.getDouble('clickingProgress') ?? 0.0;
-    _clickMultiplier = prefs.getDouble('clickMultiplier') ?? 1;
+    _clickMultiplier =
+        prefs.getDouble('clickMultiplier') ?? defaultClickMultiplier;
     _numberOfClicks = prefs.getInt('numberOfClicks') ?? 0;
     _numberOfDumplingsOpened = prefs.getInt('numberOfDumplingsOpened') ?? 0;
   }
